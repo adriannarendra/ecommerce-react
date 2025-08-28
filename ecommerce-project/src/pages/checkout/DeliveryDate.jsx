c49bbb40c1e3f4e754694c1c5b22b817da436438
Adrian Narendra <adrianenarendra@gmail.com>
2025-08-28 10:39:21 +0700

    Seperate delivery date from order summary

---
+++ ecommerce-project/src/pages/checkout/DeliveryDate.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import dayjs from 'dayjs'
+
+const DeliveryDate = ({ cartItem, deliveryOptions }) => {
+    const selectedDeliveryOption = deliveryOptions.find((deliveryOption) => {
+        return deliveryOption.id === cartItem.deliveryOptionId
+    })
+
+    return (
+        <div className="delivery-date">
+            Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
+        </div>
+    )
+}
+
+export default DeliveryDate
--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
-import dayjs from 'dayjs'
 import DeliveryOptions from './DeliveryOptions'
 import CartItemDetails from './CartItemDetails'
+import DeliveryDate from './DeliveryDate'
 
 const OrderSummary = ({ cart, deliveryOptions }) => {
     return (
         <div className="order-summary">
             {deliveryOptions.length > 0 && cart.map((cartItem) => {
-                const selectedDeliveryOption = deliveryOptions.find((deliveryOption) => {
-                    return deliveryOption.id === cartItem.deliveryOptionId
-                })
-
                 return (
                     <div key={cartItem.id} className="cart-item-container">
-                        <div className="delivery-date">
-                            Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-                        </div>
+                        <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions} />
 
                         <div className="cart-item-details-grid">
                             <CartItemDetails cartItem={cartItem} />
